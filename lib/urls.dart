5a3250546bb9204f3bbafad29c8245feb711e25b
Archer <ogunkapc@gmail.com>
2023-05-31 11:34:38 +0100

    updated urls for more requests

--- lib/urls.dart
+++ lib/urls.dart
@@ -1,12 +1,10 @@
-const ip = "192.168.100.86";
+//current ip address of local machine is:
+const ip = "192.168.224.60";
 
-const  retrieveUrl = 'http://$ip:8000/notes/';
-// final deleteUrl = Uri.parse('http://192.168.0.103:8000/notes/id/');
+final retrieveNotesUrl = Uri.parse('http://$ip:8000/notes/');
+final createUrl = Uri.parse('http://$ip:8000/notes/create/');
+updateUrl(int id) => Uri.parse('http://$ip:8000/notes/$id/update/');
+deleteUrl(int id) => Uri.parse('http://$ip:8000/notes/$id/delete/');
+retrieveNoteUrl(int id) => Uri.parse('http://$ip:8000/notes/$id/');
 // final retrieveUrl = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-// final retrieveUrl = Uri.parse('http://10.0.2.2/notes/');
 
-// retrieveUrl() {
-//   Uri.parse('http://192.168.0.103:8000/notes/');
-// }
-
-deleteUrl(int id) => 'http://$ip/notes/$id/delete';
